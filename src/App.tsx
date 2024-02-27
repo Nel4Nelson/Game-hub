ee27e56410ea1b7ce415d0a65997ce2210bca9bc
nel4nelson <ekwuememichael131@gmail.com>
2024-02-27 22:13:03 +0100

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <div>
-      <h1>Game Hub</h1>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="blueviolet">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="gold">
+          Main
+        </GridItem>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
