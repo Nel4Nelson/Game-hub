83dde9a2cef2249fdeddd078d4344c68d83aabc8
nel4nelson <ekwuememichael131@gmail.com>
2024-02-28 21:30:35 +0100

    displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,18 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Games {
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
+export interface Games {
   id: number;
   name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGameResponse {
