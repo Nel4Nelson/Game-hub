83c8d125ac33d6f2dd5b6c8e265298a11fbe3f8a
nel4nelson <ekwuememichael131@gmail.com>
2024-03-01 13:53:40 +0100

    build sort selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = ({gameQuery}: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
-    <Box mt={4}>
+    <Box mt={5}>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3}} spacing={3}>
         {isLoading &&
